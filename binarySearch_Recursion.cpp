19662ef7855676c827a3fe27d806be728c7bc397
Gajal Garg <gajalgarg09@gmail.com>
2024-08-15 13:17:39 +0530

    Add files via upload

---
+++ binarySearch_Recursion.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+
+bool binarySearch(int arr[] , int s , int e , int key){
+
+    if( s>e)
+        return false;
+
+    int mid = s + (e-s)/2;
+
+    if(arr[mid] == key )
+        return true;
+
+    if( arr[mid] > key){
+        return binarySearch( arr, s , mid-1 , key);
+    }
+    else{
+        return binarySearch( arr , mid+1 , e, key);
+    }
+}
+
+int main(){
+
+    int arr[6] = {1,2,3,4,5,6};
+    int size = 5;
+    int key = 7;
+
+    cout << "Present or not : " << binarySearch( arr , 0 , 5 , key);
+
+
+
+
+    return 0;
+}
