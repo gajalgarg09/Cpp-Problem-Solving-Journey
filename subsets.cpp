5ef4ae34f6e3395ddf332e37b26fc479e0c70a9e
Gajal Garg <gajalgarg09@gmail.com>
2024-08-16 15:04:56 +0530

    Update subsets.cpp

--- subsets.cpp
+++ subsets.cpp
@@ -1,12 +1,10 @@
 // Given an integer array nums of unique elements, return all possible 
 // subsets(the power set).
-
 // The solution set must not contain duplicate subsets. Return the solution in any order.
 
 #include<iostream>
 #include<vector>
 using namespace std;
-
 
 class Solution {
 public:
