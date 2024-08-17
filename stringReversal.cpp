11502aa1f4a558020fd5178c5c4ef1aa9c66594a
Gajal Garg <gajalgarg09@gmail.com>
2024-08-17 11:44:47 +0530

    Add files via upload

---
+++ stringReversal.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        int n = s.size();
+        int i=0;
+        while(i<n/2){
+            swap(s[i] , s[n-i-1]);
+            i++;
+        }  
+    }
+};
