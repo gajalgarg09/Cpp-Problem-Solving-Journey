304c3a408bc176cbfe11e7cb08f5015af61cfa8d
Gajal Garg <gajalgarg09@gmail.com>
2024-08-17 11:42:28 +0530

    Update length_of_last_word.cpp

--- length_of_last_word.cpp
+++ length_of_last_word.cpp
@@ -1,3 +1,6 @@
+// Given a string s consisting of words and spaces, return the length of the last word in the string.
+// A word is a maximal substring consisting of non-space characters only.
+
 #include<iostream>
 using namespace std;
 
