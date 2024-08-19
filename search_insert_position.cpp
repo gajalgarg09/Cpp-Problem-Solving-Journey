77711589b580f94518144cb12159e54faa72b5af
Gajal Garg <gajalgarg09@gmail.com>
2024-08-19 13:29:35 +0530

    Update search_insert_position.cpp

--- search_insert_position.cpp
+++ search_insert_position.cpp
@@ -1,3 +1,10 @@
+// Problem Statement
+// Given a sorted array of distinct integers and a target value, return the index if the target is found.
+// If not, return the index where it would be if it were inserted in order.
+// You must write an algorithm with O(log n) runtime complexity.
+
+// We've used the concept of Binary Search in the question.
+
 #include<iostream>
 #include<vector>
 using namespace std;
