6c8d004f4040c6c3b4bb8ee125340b4de60cc3ab
Gajal Garg <gajalgarg09@gmail.com>
2024-09-17 17:19:51 +0530

    Update max_product_of3.cpp

--- max_product_of3.cpp
+++ max_product_of3.cpp
@@ -1,3 +1,12 @@
+// Given an integer array nums, find three numbers whose product is maximum and return the maximum product.
+// Example 1:
+// Input: nums = [1,2,3]
+// Output: 6
+
+// Example 2:
+// Input: nums = [-1,-2,-3]
+// Output: -6
+
 #include <iostream>
 #include <vector>
 #include <algorithm>
