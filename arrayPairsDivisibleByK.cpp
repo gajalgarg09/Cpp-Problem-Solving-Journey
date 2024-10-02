c020498370667144608e9994a3436b67fd23c088
Gajal Garg <gajalgarg09@gmail.com>
2024-10-02 09:56:15 +0530

    Update arrayPairsDivisibleByK.cpp

--- arrayPairsDivisibleByK.cpp
+++ arrayPairsDivisibleByK.cpp
@@ -1,3 +1,8 @@
+// Given an array of integers arr of even length n and an integer k.
+// We want to divide the array into exactly n / 2 pairs such that the sum of each pair is divisible by k.
+// Return true If you can find a way to do that or false otherwise.
+
+
 #include<iostream>
 #include<vector>
 #include<algorithm>
