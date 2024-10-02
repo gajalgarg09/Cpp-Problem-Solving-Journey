0010152d2e0e90937d2fb90565bba6b4fc10c67f
Gajal Garg <gajalgarg09@gmail.com>
2024-10-02 09:53:19 +0530

    Update rank_transform.cpp

--- rank_transform.cpp
+++ rank_transform.cpp
@@ -1,3 +1,19 @@
+// Problem Statement
+// Given an array of integers arr, replace each element with its rank.
+// The rank represents how large the element is. The rank has the following rules:
+// Rank is an integer starting from 1.
+// The larger the element, the larger the rank. If two elements are equal, their rank must be the same.
+// Rank should be as small as possible.
+
+// Solution Intuition
+// we will create duplicate array
+// we will sort it
+// we wil use hashmap for faster lookup times
+// we will start the ranking from the smallest with rank 1 and go on increasing the
+// rank by one
+// finally we will return the array with the values from the stored hashmap according
+// to the input array order
+
 #include<iostream>
 #include<vector>
 #include<map>
