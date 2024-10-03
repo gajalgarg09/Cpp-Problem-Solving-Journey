bd89b25eef91362ba659fb8c67bc6fd71efc03f2
Gajal Garg <gajalgarg09@gmail.com>
2024-10-03 12:12:33 +0530

    Update twoStacks.cpp

--- twoStacks.cpp
+++ twoStacks.cpp
@@ -1,3 +1,18 @@
+// Design a data structure, which represents two stacks using only one array common for both stacks. The data structure should support the following operations:
+
+// push1(NUM) - Push ‘NUM’ into stack1.
+// push2(NUM) - Push ‘NUM’ into stack2.
+// pop1() - Pop out a top element from stack1 and return popped element, in case of underflow return -1.
+// pop2() - Pop out a top element from stack2 and return popped element, in case of underflow return -1.
+// There are 2 types of queries in the input
+
+// Type 1 - These queries correspond to Push operation.
+// Type 2 - These queries correspond to Pop operation.
+// Note:
+// 1. You are given the size of the array.
+// 2. You need to perform push and pop operations in such a way that we are able to push elements in the stack until there is some empty space available in the array.
+// 3. While performing Push operations, do nothing in the situation of the overflow of the stack.
+
 #include<iostream>
 using namespace std;
 class TwoStack {
