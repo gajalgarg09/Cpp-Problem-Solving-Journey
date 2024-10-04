b25a14f8e80c207feae3d99f2ca2ef59f6b607af
Gajal Garg <gajalgarg09@gmail.com>
2024-10-04 19:59:09 +0530

    Update dividePlayersIntoTeamsOfEqualSkill.cpp

--- dividePlayersIntoTeamsOfEqualSkill.cpp
+++ dividePlayersIntoTeamsOfEqualSkill.cpp
@@ -1,3 +1,8 @@
+// You are given a positive integer array skill of even length n where skill[i] denotes the skill of the ith player.
+// Divide the players into n / 2 teams of size 2 such that the total skill of each team is equal.
+// The chemistry of a team is equal to the product of the skills of the players on that team.
+// Return the sum of the chemistry of all the teams, or return -1 if there is no way to divide the players into teams such that the total skill of each team is equal.
+
 #include<iostream>
 #include<vector>
 #include<algorithm>
