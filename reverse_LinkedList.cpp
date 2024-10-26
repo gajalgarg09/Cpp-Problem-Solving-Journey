8e78d3cc36f45a8f4a66b0054172837b3d6861bb
Gajal Garg <gajalgarg09@gmail.com>
2024-10-26 13:13:09 +0530

    Add files via upload

---
+++ reverse_LinkedList.cpp
@@ -0,0 +1,77 @@
+#include<iostream>
+using namespace std;
+
+class Node{
+    public:
+        int data;
+        Node* next; 
+
+// CONSTRUCTOR
+    Node(int data){
+        this-> data = data;
+        this-> next = NULL;
+    }
+};
+
+// ITERATIVE SOLUTION   (APPROACH -1)
+Node* reverseLinkedList(Node* head){
+    if( head == NULL || head-> next == NULL){
+        return head;
+    }
+
+    Node* prev = NULL;
+    Node*  curr = head;
+    Node* forward = NULL;
+
+    while( curr != NULL){
+        forward = curr -> next;
+        curr -> next = prev;
+        prev = curr;
+        curr= forward;
+    }
+    return prev;
+}
+
+
+
+// RECURSIVE SOLUTION  (APPROACH-2)
+void reverse(Node* head , Node* curr , Node* prev){
+
+    if( curr == NULL){
+        head = prev;
+        return;
+    }
+
+    Node* forward = curr -> next;
+    reverse( head , forward , curr);
+    curr -> next = prev;
+
+}
+
+Node* reverseLinkedList(Node* head){
+
+    Node* curr = head;
+    Node* prev = NULL;
+    reverse( head , curr , prev);
+    return head;
+}
+
+
+// RECURSIVE SOLUTION  (APPROACH-3)
+
+// it will return head of reverse list
+Node* reverse1(Node* head){
+    // base case
+    if( head == NULL || head-> next == NULL){
+        return head;
+    }
+
+    Node* chotaHead = reverse1( head -> next);
+    head -> next -> next = head;
+    head -> next = NULL;
+    return chotaHead;
+}
+
+Node* reverseLinkedList(Node* head){
+    return reverse1(head);
+}
