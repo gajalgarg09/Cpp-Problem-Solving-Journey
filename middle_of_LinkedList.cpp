e6fcb452d2bf26e16f3e58c339281ad6bc5238bb
Gajal Garg <gajalgarg09@gmail.com>
2024-10-26 13:08:26 +0530

    Update middle_of_LinkedList.cpp

--- middle_of_LinkedList.cpp
+++ middle_of_LinkedList.cpp
@@ -1,3 +1,6 @@
+// Given the head of a singly linked list, return the middle node of the linked list.
+// If there are two middle nodes, return the second middle node.
+
 #include<iostream>
 using namespace std;
 
