44d7a42039427b3397d3d5785eea132276dda1ed
Gajal Garg <gajalgarg09@gmail.com>
2024-10-26 18:34:40 +0530

    Update reverseLL_in_K_groups.cpp

--- reverseLL_in_K_groups.cpp
+++ reverseLL_in_K_groups.cpp
@@ -1,3 +1,15 @@
+// You are given a linked list of 'n' nodes and an integer 'k', where 'k' is less than or equal to 'n'.
+// Your task is to reverse the order of each group of 'k' consecutive nodes, if 'n' is not divisible by 'k', then the last group of nodes should remain unchanged.
+
+// For example, if the linked list is 1->2->3->4->5, and 'k' is 3, we have to reverse the first three elements, 
+// and leave the last two elements unchanged. Thus, the final linked list being 3->2->1->4->5.
+
+// Implement a function that performs this reversal, and returns the head of the modified linked list.
+
+// Example:
+// Input: 'list' = [1, 2, 3, 4], 'k' = 2
+// Output: 2 1 4 3
+
 #include<iostream>
 using namespace std;
 
