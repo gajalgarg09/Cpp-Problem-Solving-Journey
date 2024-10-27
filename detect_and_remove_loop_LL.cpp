efe10f6619857ea2333bc6481792bc30d4b50afa
Gajal Garg <gajalgarg09@gmail.com>
2024-10-27 13:15:44 +0530

    Update detect_and_remove_loop_LL.cpp

--- detect_and_remove_loop_LL.cpp
+++ detect_and_remove_loop_LL.cpp
@@ -1,3 +1,8 @@
+// Given a singly linked list, you have to detect the loop and remove the loop from the linked list, 
+// if present. You have to make changes in the given linked list itself and return the updated linked list.
+// Expected Complexity: Try doing it in O(n) time complexity and O(1) space complexity. Here, n is the number of nodes in the linked list.
+
+
 #include<iostream>
 #include<map>
 using namespace std;
