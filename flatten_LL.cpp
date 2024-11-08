6d076bf6730a33de2b1d3afb121d673b6e965266
Gajal Garg <gajalgarg09@gmail.com>
2024-11-08 12:39:59 +0530

    Update flatten_LL.cpp

--- flatten_LL.cpp
+++ flatten_LL.cpp
@@ -1,3 +1,9 @@
+// You are given a linked list containing 'n' 'head' nodes, where every node in the linked list contains two pointers:
+// (1) ‘next’ which points to the next node in the list
+// (2) ‘child’ pointer to a linked list where the current node is the head.
+// Each of these child linked lists is in sorted order and connected by 'child' pointer.
+// Your task is to flatten this linked such that all nodes appear in a single layer or level in a 'sorted order'.
+
 #include<iostream>
 using namespace std;
 
