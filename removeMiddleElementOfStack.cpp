4f680a861dff4d16ff027e46ff14c78c5f9078b0
Gajal Garg <gajalgarg09@gmail.com>
2024-11-10 12:47:47 +0530

    Add files via upload

---
+++ removeMiddleElementOfStack.cpp
@@ -0,0 +1,22 @@
+#include <iostream> 
+#include<stack>
+using namespace std;
+
+void solve(stack<int>&inputStack , int count,  int size){
+    if( count == size/2){
+        inputStack.pop();
+        return ;
+    }
+
+    int num = inputStack.top();
+    inputStack.pop();
+
+    solve(inputStack , count+1, size);
+
+    inputStack.push(num);
+}
+
+void deleteMiddleElement(stack<int>&inputStack , int n){
+    int count = 0;
+    solve(inputStack , count , n);
+}
