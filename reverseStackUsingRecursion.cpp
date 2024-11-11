cc0ce2d26ffb75ff7baf76e229266d44f96d3eae
Gajal Garg <gajalgarg09@gmail.com>
2024-11-11 15:00:29 +0530

    Update reverseStackUsingRecursion.cpp

--- reverseStackUsingRecursion.cpp
+++ reverseStackUsingRecursion.cpp
@@ -1,3 +1,6 @@
+// Reverse a given stack of 'N' integers using recursion. You are required to make changes in the input parameter itself.
+// Note: You are not allowed to use any extra space other than the internal stack space used due to recursion.
+
 #include<iostream>
 #include<stack>
 using namespace std;
