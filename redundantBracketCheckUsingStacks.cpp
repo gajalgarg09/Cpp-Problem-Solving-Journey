326a32100221542556df7cffbe31814d0c0c9f56
Gajal Garg <gajalgarg09@gmail.com>
2024-11-11 15:47:36 +0530

    Update redundantBracketCheckUsingStacks.cpp

--- redundantBracketCheckUsingStacks.cpp
+++ redundantBracketCheckUsingStacks.cpp
@@ -1,3 +1,13 @@
+// Given valid mathematical expressions in the form of a string. You are supposed to return true if the given expression contains a pair of redundant brackets, else return false.
+// The given string only contains ‘(‘, ’)’, ‘+’, ‘-’, ‘*’, ‘/’ and lowercase English letters.
+
+// Note :
+// A pair of brackets is said to be redundant when a subexpression is surrounded by needless/ useless brackets.
+
+// For Example :
+// ((a+b)) has a pair of redundant brackets. The pair of brackets on the first and last index is needless. 
+// While (a + (b*c)) does not have any pair of redundant brackets. 
+
 #include<iostream>
 #include<stack>
 using namespace std;
