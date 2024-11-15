85a927364d7e2440699345f93505ace41b614096
Gajal Garg <gajalgarg09@gmail.com>
2024-11-15 20:45:56 +0530

    Add files via upload

---
+++ removeOutermostParenthesis.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<string>
+using namespace std;
+class Solution {
+public:
+    string removeOuterParentheses(string s) {
+        string ans;
+        int balance = 0;
+
+        for(int i =0; i<s.length(); i++){
+
+            if( s[i] == '('){
+                if(balance > 0){
+                    ans += s[i];
+                }
+                balance++;
+            }
+            else{
+                balance--; 
+                if(balance > 0){
+                    ans += s[i];
+                }      
+            }    
+        }
+        return ans;
+    }
+};
