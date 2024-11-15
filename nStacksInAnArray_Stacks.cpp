18867d0ade2fe65ccd62ce273766cb741ae92a82
Gajal Garg <gajalgarg09@gmail.com>
2024-11-16 01:43:48 +0530

    Update nStacksInAnArray_Stacks.cpp

--- nStacksInAnArray_Stacks.cpp
+++ nStacksInAnArray_Stacks.cpp
@@ -1,3 +1,14 @@
+// Design a data structure to implement ‘N’ stacks using a single array of size ‘S’. 
+// It should support the following operations:
+// push(X, M): Pushes an element X into the Mth stack.
+// Returns true if the element is pushed into the stack, otherwise false.
+// pop(M): Pops the top element from Mth Stack. Returns -1 if the stack is empty, 
+// otherwise, returns the popped element.
+// Two types of queries denote these operations :
+// Type 1: for push(X, M) operation.
+// Type 2: for pop(M) operation.
+
+    
 #include<iostream>
 #include<stack>
 using namespace std;
