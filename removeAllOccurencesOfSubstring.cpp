e953e38f22c7975e9dabf1a02e3895038c48b71f
Gajal Garg <gajalgarg09@gmail.com>
2024-12-27 16:37:01 +0530

    Update removeAllOccurencesOfSubstring.cpp

--- removeAllOccurencesOfSubstring.cpp
+++ removeAllOccurencesOfSubstring.cpp
@@ -1,3 +1,8 @@
+// Given two strings s and part, perform the following operation on s until all occurrences of the substring part are removed:
+// Find the leftmost occurrence of the substring part and remove it from s.
+// Return s after removing all occurrences of part.
+// A substring is a contiguous sequence of characters in a string.
+
 #include<iostream>
 #include<string>
 using namespace std;
