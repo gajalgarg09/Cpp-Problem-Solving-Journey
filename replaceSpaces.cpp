7a99f3af25107811b8c179a48a6d1c040383662d
Gajal Garg <gajalgarg09@gmail.com>
2024-12-27 16:17:36 +0530

    Add files via upload

---
+++ replaceSpaces.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+string replaceString(string &str){
+
+    string temp = "";
+    for(int i =0; i<str.length(); i++){
+        if(str[i] == ' '){
+            temp.push_back('@');
+            temp.push_back('4');
+            temp.push_back('0');
+        }
+        else{
+            temp.push_back(str[i]);
+        }
+    }
+    return temp;
+}
+
+int main() {
+    string s;
+    cout << "Enter the string:" << endl;
+    getline(cin, s); 
+
+    cout << "You entered: " << s << endl;
+
+    string newString = replaceString(s); 
+    cout << "New String: " << newString << endl;
+
+    return 0;
+}
