1c227d9678a34e764dbfbcf0f8224000125627e4
Gajal Garg <gajalgarg09@gmail.com>
2024-12-27 15:58:19 +0530

    Update mostFrequentCharInString.cpp

--- mostFrequentCharInString.cpp
+++ mostFrequentCharInString.cpp
@@ -1,3 +1,11 @@
+// Given a string s of lowercase alphabets. The task is to find the maximum occurring character in the string s. 
+// If more than one character occurs the maximum number of times then print the lexicographically smaller character.
+
+// Examples:
+// Input: s = "testsample"
+// Output: 'e'
+// Explanation: e is the character which is having the highest frequency.
+
 #include<iostream>
 using namespace std;
 
