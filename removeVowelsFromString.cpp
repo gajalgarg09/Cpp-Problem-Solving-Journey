687fef3050c79f953b5bbc782c39476bcaf27a36
Gajal Garg <gajalgarg09@gmail.com>
2025-01-02 12:27:34 +0530

    Update removeVowelsFromString.cpp

--- removeVowelsFromString.cpp
+++ removeVowelsFromString.cpp
@@ -1,3 +1,10 @@
+// You are given a string STR of length N. Your task is to remove all the vowels present in that string and print the modified string.
+// English alphabets ‘a’, ‘e’, ‘i’, ‘o’, ‘u’ are termed as vowels. All other alphabets are called consonants.
+// Note: You have to only remove vowels from the string. There will be no change in the relative position of all other alphabets.
+// For example:
+// (i)  If the input string is 'CodeGeek', the output should be CdGk after removing ‘o’ and ‘e’.
+// (ii) If the input string is 'Odinson', the output should be 'dnsn' after removing ‘o’ and ‘i’. 
+
 #include<iostream>
 #include<string>
 using namespace std;
