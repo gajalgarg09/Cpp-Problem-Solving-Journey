ea44737f5a1bdb40d874dfab86f07ce2131565ce
Gajal Garg <gajalgarg09@gmail.com>
2025-02-05 10:08:37 +0530

    Update peakIndexInMountainArray.cpp

--- peakIndexInMountainArray.cpp
+++ peakIndexInMountainArray.cpp
@@ -1,3 +1,7 @@
+// You are given an integer mountain array arr of length n where the values increase to a peak element and then decrease.
+// Return the index of the peak element.
+// Your task is to solve it in O(log(n)) time complexity.
+
 #include<iostream>
 #include<vector>
 using namespace std;
