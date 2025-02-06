7a6434eb8b84f000cd6effbbac34585a19224cb5
Gajal Garg <gajalgarg09@gmail.com>
2025-02-06 15:44:00 +0530

    Update squareRootUsingBinarySearch.cpp

--- squareRootUsingBinarySearch.cpp
+++ squareRootUsingBinarySearch.cpp
@@ -1,3 +1,7 @@
+// You are given a positive integer ‘n’.
+// Your task is to find and return its square root. 
+// If ‘n’ is not a perfect square, then return the floor value of sqrt(n).
+
 #include<iostream>
 using namespace std;
 
