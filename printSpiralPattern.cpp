53a976530e8f50fb84861a8b34d2f9789c001a23
Gajal Garg <gajalgarg09@gmail.com>
2025-02-07 13:26:46 +0530

    Add files via upload

---
+++ printSpiralPattern.cpp
@@ -0,0 +1,52 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+vector<int> spiralOrder(vector<vector<int>>& matrix){
+
+    vector<int> ans;
+
+    int row = matrix.size();
+    int col = matrix[0].size();
+
+    int count = 0;
+    int total = row*col;
+
+    int startCol = 0;
+    int startRow = 0;
+    int endCol = col -1;
+    int endRow = row-1;
+
+    while(count < total){
+
+        // printing the first row
+        for(int index = startCol; count < total &&  index <=endCol; index++){
+            ans.push_back(matrix[startRow][index]);
+            count++;
+        }
+        startRow++;
+
+        // printing the last column
+        for(int index = startRow;count < total &&  index <= endRow; index++){
+            ans.push_back(matrix[index][endCol]);
+            count++;
+        }
+        endCol--;
+
+        // printing the last row
+        for(int index = endCol ; count < total && index >= startCol; index--){
+            ans.push_back(matrix[endRow][index]);
+            count++;
+        }
+        endRow--;
+
+        // printing the first column
+        for(int index = endRow; count < total && index >= startRow; index--){
+            ans.push_back(matrix[index][startCol]);
+            count++;
+        }
+        startCol++;
+    }
+
+    return ans;
+}
