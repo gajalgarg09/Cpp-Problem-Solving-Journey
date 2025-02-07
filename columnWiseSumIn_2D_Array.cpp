8f35a87efe4023eb23bb8eecc49eddadf66308d2
Gajal Garg <gajalgarg09@gmail.com>
2025-02-07 11:12:23 +0530

    Add files via upload

---
+++ columnWiseSumIn_2D_Array.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+using namespace std;
+
+int colSum(int arr[][3] , int row , int col){
+    cout << "Column wise sum : "  << endl;
+    for(int col = 0; col < 3; col++){
+        int sum = 0;
+        for(int row = 0; row < 3; row++){
+            sum += arr[row][col];
+        }
+        cout << sum << " ";
+    }
+    cout << endl;
+}
+
+
+int main(){
+    int arr[3][3];
+
+    cout << "Enter the elements of Array :" << endl;
+    for(int i =0; i<3; i++){
+        for(int j = 0; j<3; j++){
+            cin >> arr[i][j];
+        }
+    }
+
+    cout << "Printing the Array : " << endl;
+    for(int i = 0; i<3; i++){
+        for(int j = 0; j<3; j++){
+            cout <<  arr[i][j];
+        }
+        cout << endl;
+    }
+
+    colSum(arr , 3, 3 );
+    return 0;
+}
