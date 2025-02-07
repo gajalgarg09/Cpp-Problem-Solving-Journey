c7d6798f3522202f2dc3c23efff890c4731f573f
Gajal Garg <gajalgarg09@gmail.com>
2025-02-07 13:27:37 +0530

    Update printSpiralPattern.cpp

--- printSpiralPattern.cpp
+++ printSpiralPattern.cpp
@@ -1,3 +1,11 @@
+//Given an m x n matrix, return all elements of the matrix in spiral order.
+
+// Input: matrix = [[1,2,3],[4,5,6],[7,8,9]]
+// 1 -> 2 -> 3 -> 6 -> 9 -> 8 -> 7 -> 4 -> 5
+// Output: [1,2,3,6,9,8,7,4,5]
+
+// O(n*m)
+
 #include<iostream>
 #include<vector>
 using namespace std;
