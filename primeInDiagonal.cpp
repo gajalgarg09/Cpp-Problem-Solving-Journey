1c33395c8d7ca58c9b8dd298d8c0da7365a34c9d
Gajal Garg <gajalgarg09@gmail.com>
2025-02-09 15:26:17 +0530

    Update primeInDiagonal.cpp

--- primeInDiagonal.cpp
+++ primeInDiagonal.cpp
@@ -1,3 +1,14 @@
+// You are given a 0-indexed two-dimensional integer array nums.
+// Return the largest prime number that lies on at least one of the diagonals of nums. In case, no prime is present on any of the diagonals, return 0.
+// Note that:
+// An integer is prime if it is greater than 1 and has no positive integer divisors other than 1 and itself.
+// An integer val is on one of the diagonals of nums if there exists an integer i for which nums[i][i] = val or an i for which nums[i][nums.length - i - 1] = val.
+
+// Time comlexity = O(N* (root M))
+// N -> size of matrix
+// M -> maximum element
+
+
 #include<iostream>
 #include<vector>
 using namespace std;
