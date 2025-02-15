58287f22879c929c63d4c4ebd98a4af5b8b06032
Gajal Garg <gajalgarg09@gmail.com>
2025-02-15 14:35:04 +0530

    Update leftViewOfBinaryTree.cpp

--- leftViewOfBinaryTree.cpp
+++ leftViewOfBinaryTree.cpp
@@ -1,3 +1,7 @@
+// You are given the root of a binary tree. Your task is to return the left view of the binary tree. 
+// The left view of a binary tree is the set of nodes visible when the tree is viewed from the left side.
+// If the tree is empty, return an empty list.
+
 #include<iostream>
 #include<vector>
 #include<unordered_map>
