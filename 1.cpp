bd5ab82e1c01c76ce1c99ed1c0e1e81a5551eefc
Gajal Garg <gajalgarg09@gmail.com>
2025-02-18 14:58:56 +0530

    Add files via upload

---
+++ 1.cpp
@@ -0,0 +1,85 @@
+#include<iostream>
+using namespace std;
+
+// For a 32 bit system size of pointer is 4 bytes
+// For a 64 bit system size of pointer is 64 bytes
+
+int main(){
+
+    // int a = 5;
+    // cout << a << endl;
+
+    // // address operator -> &
+    // cout << "Address of a is : " << &a << endl;
+
+    // int *ptr = &a;
+    // //cout << "Value is : " << ptr << endl;
+
+    // cout << "Address : " << ptr << endl;
+    // cout << "Value : " << *ptr << endl;
+    // cout << "Size of integer is  : " << sizeof(a) << endl;
+    // cout << "Size of pointer is  : " << sizeof(ptr) << endl;
+    
+
+    // double d = 4.22;
+    // double *pt2 = &d;
+    // cout << "Size of double is  : " << sizeof(d) << endl;
+    // cout << "Size of pointer is  : " << sizeof(pt2) << endl;
+
+    //int *p;          // bad practice
+    // instead do
+
+    // int *p = 0;
+    // cout << *p  << endl;
+
+    // both will give same answer
+    // int i = 5;
+    // int *q = &i;
+    // cout << q << endl;
+    // cout << *q << endl;
+
+    // int *pt = &i;
+    // cout << pt << endl;
+    // cout << *pt << endl;
+
+
+    // int num = 5;
+    // int a = num;
+    // a++;
+    // cout << a << endl;
+    // cout << num << endl;
+
+    
+    // int *p = &num;
+    // cout << "Before :" << num << endl;
+    // (*p)++;
+    // cout << "After :" << num << endl;
+
+    // copying pointers
+    // int a = 5;
+    // int *p = &a;
+    // int *q = p;
+    // cout << p << " - " << q << endl;
+    // cout << *p << " - " << *q << endl;
+
+    // Important concept
+    int a = 5;
+    int *p = &a;
+    (*p)++;
+    cout << a << endl;
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+}
+
