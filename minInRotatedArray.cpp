ae3d758eb5d118dfb103d5893e309e94009b8bf5
Gajal Garg <gajalgarg09@gmail.com>
2025-02-19 09:54:10 +0530

    Add files via upload

---
+++ minInRotatedArray.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int findMin(vector<int> nums){
+
+    int s = 0;
+    int e = nums.size()-1;
+
+    while ( s < e){
+        int mid = s + (e-s)/2;
+
+        if( nums[mid] < nums[e]){
+            e = mid; // min on left side
+        }
+        else{
+            s = mid + 1;
+        }
+    }
+    return nums[s];
+}
