758354416f10bc897ca75e4d298de3d32bb9493d
Gajal Garg <gajalgarg09@gmail.com>
2025-02-20 22:30:27 +0530

    Update isSortedArray_Recursion.cpp

--- isSortedArray_Recursion.cpp
+++ isSortedArray_Recursion.cpp
@@ -1,8 +1,9 @@
+// Check if the array is sorted using Recursion.
+
 #include<iostream>
 using namespace std;
 
 bool isSorted(int arr[] , int size){
-
     if(size == 0 || size == 1){
         return true;
     }
