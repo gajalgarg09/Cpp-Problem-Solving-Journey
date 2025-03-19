788c2a4a7dcf24290837d1e8dfa71f99daf4aa0e
Gajal Garg <gajalgarg09@gmail.com>
2025-03-19 10:08:36 +0530

    Add files via upload

---
+++ minimum_operations.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Solution{
+    public:
+        int minOperations(vector<int>& nums){
+            int n = nums.size();
+            int flips = 0;
+
+            for(int i =0; i<= n-3; i++){
+                if( nums[i] == 0){
+                    nums[i] ^= 1;
+                    nums[i+1] ^= 1;
+                    nums[i+2] ^= 1;
+                    flips++;
+                }
+            }
+            for (int i = n-2; i < n; i++){
+                if( i >= 0 && nums[i] == 0){
+                    return -1;
+                }
+            }
+            return flips;
+        }
+};
