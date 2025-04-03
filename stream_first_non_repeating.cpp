b60ea4d1ed2ff3d8aeec18bca6ce7d50edf7c3fe
Gajal Garg <gajalgarg09@gmail.com>
2025-04-03 11:43:27 +0530

    Update stream_first_non_repeating.cpp

--- stream_first_non_repeating.cpp
+++ stream_first_non_repeating.cpp
@@ -1,3 +1,11 @@
+// Given an input stream s consisting only of lowercase alphabets. While reading characters from the stream, 
+// you have to tell which character has appeared only once in the stream upto that point. 
+// If there are many characters that have appeared only once, you have to tell which one of them was the first one to appear.
+// If there is no such character then append '#' to the answer.
+// NOTE:
+// 1. You need to find the answer for every i (0 <= i < n)
+// 2. In order to find the solution for every you need to consider the string from starting position till the ith position.
+
 #include<iostream>
 #include<unordered_map>
 #include<queue>
