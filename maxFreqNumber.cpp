960002de42cd9daacb7085287106d6277dd6099a
Gajal Garg <gajalgarg09@gmail.com>
2025-06-04 22:10:09 +0530

    Update maxFreqNumber.cpp

--- maxFreqNumber.cpp
+++ maxFreqNumber.cpp
@@ -1,3 +1,9 @@
+// Ninja is given an array of integers that contain numbers in random order. He needs to write a program to find and return the number 
+// which occurs the maximum times in the given input. He needs your help to solve this problem.
+// If two or more elements contend for the maximum frequency, return the element which occurs in the array first i.e. whose index is lowest.
+// For example,
+// For 'arr' = [ 1, 2, 3, 1, 2]. you need to return 1.
+
 #include<iostream>
 #include<unordered_map>
 #include<vector>
