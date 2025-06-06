d8814920aae0b86a5b5e4f57c2d07a9f4e9d49d1
Gajal Garg <gajalgarg09@gmail.com>
2025-06-06 20:11:09 +0530

    Update similarityBetweenArrays.cpp

--- similarityBetweenArrays.cpp
+++ similarityBetweenArrays.cpp
@@ -1,10 +1,11 @@
+// You have been given two arrays/list ‘ARR1’ and ‘ARR2’ consisting of ‘N’ and ‘M’ integers respectively. 
+// Your task is to return the number of elements common to ‘ARR1’ and ‘ARR2’ and the number of elements in the union of ‘ARR1’ and ‘ARR2’.
+
 #include<iostream>
 #include<unordered_map>
 using namespace std;
 
-
 pair < int , int > findSimilarity(vector < int > arr1, vector < int > arr2, int n, int m) {
-
     unordered_map<int , int> mp;
     int intersection = 0;
 
