a30a7c68fa4926ba3f5f21f8d87604d2ed89557d
Gajal Garg <gajalgarg09@gmail.com>
2025-06-09 19:54:15 +0530

    Add files via upload

---
+++ findPairGivenDifference.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+bool findPair(vector<int> nums, int target){
+
+    int n = nums.size();
+    sort(nums.begin() , nums.end());
+
+    int j = 1;
+
+    for(int i =0; i<n; i++){
+
+        while(j <n && nums[j] - nums[i] < target){
+            j++;
+        }
+
+        if(j<n && i!=j && nums[j]-nums[i == target]){
+            return true;
+        }
+    }
+    return false;
+}
