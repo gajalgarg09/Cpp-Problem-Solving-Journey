2d4b2bf8228964754ebd0e2f6c565e3c9cd3efd9
Gajal Garg <gajalgarg09@gmail.com>
2025-06-11 17:51:12 +0530

    Add files via upload

---
+++ scoreOfAString.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+class Solution {
+public:
+    int scoreOfString(string s) {
+        int score = 0;
+        for(int i =0; i<s.size()-1; i++){
+            score = score + abs((int)s[i] - (int)s[i+1]);
+        }
+        return score;
+    }
+};
