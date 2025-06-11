205e396bf186ff36daac58d3d577a2c7b2505311
Gajal Garg <gajalgarg09@gmail.com>
2025-06-11 17:52:11 +0530

    Update scoreOfAString.cpp

--- scoreOfAString.cpp
+++ scoreOfAString.cpp
@@ -1,3 +1,8 @@
+// You are given a string s. The score of a string is defined as the 
+// sum of the absolute difference between the ASCII values of adjacent characters.
+// Return the score of s.
+
+
 #include<iostream>
 #include<string>
 using namespace std;
