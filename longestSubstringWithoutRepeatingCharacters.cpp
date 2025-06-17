7a1693fff819a918f9f9780dc79aa014437d8d9a
Gajal Garg <gajalgarg09@gmail.com>
2025-06-17 16:26:50 +0530

    Update longestSubstringWithoutRepeatingCharacters.cpp

--- longestSubstringWithoutRepeatingCharacters.cpp
+++ longestSubstringWithoutRepeatingCharacters.cpp
@@ -1,14 +1,14 @@
+// Given a string s, find the length of the longest substring without duplicate characters.
+
 #include<iostream>
 #include<vector>
 using namespace std;
 
 int lengthOfLongestSubstring(string s){
-
-    int n = s.length();
-    int l =0, r =0, maxLen = 0;
-
     vector<int> hashArray(255, -1);
-
+    int n = s.length();
+    int l = 0, r = 0, maxLen = 0;
+   
     while(r < n){
         if(hashArray[s[r]] != -1){
             if(hashArray[s[r]] >= l){
